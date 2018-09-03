e1de8b315af1eb2295f2dec080c1bfedcbcea4e8
Douglas Fortunato <douglas.fortunato@southsystem.com.br>
2018-09-03 17:17:16 -0300

    Improves redux structure.

--- frontend/src/Redux/Actions/category.js
+++ frontend/src/Redux/Actions/category.js
@@ -1,8 +1,23 @@
 import {GET_CATEGORIES} from './types';
 
-export function addPost(categories){
-    return {
-        type: GET_CATEGORIES,
-        categories
+export const addPost = (categories =  [
+    {
+        name:'React',
+        path:'react'
+    },
+    {
+        name:'Redux',
+        path:'redux'
+    },
+    {
+        name:'Android',
+        path:'android'
+    },
+    {
+        name:'Linux',
+        path:'linux'
     }
-}
+]) => ({
+    type: GET_CATEGORIES,
+    categories
+})
--- frontend/src/Redux/Reducers/categoryReducer.js
+++ frontend/src/Redux/Reducers/categoryReducer.js
@@ -3,7 +3,11 @@ import {GET_CATEGORIES} from '../Actions/types';
 export default (state = {}, action) => {
     switch(action.type){
         case GET_CATEGORIES:
-            return;
+            return{
+                ...state,
+                categories: action.categories
+            };
+        default:
+            return state;
     }
-    return state
 }
