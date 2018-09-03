eb79f0ead55a1476094777d3eb599104c93c045f
Douglas Fortunato <douglas.fortunato@southsystem.com.br>
2018-09-03 17:30:02 -0300

    Add map to props.

--- frontend/src/Components/Ui/Menu.js
+++ frontend/src/Components/Ui/Menu.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { connect } from "react-redux";
 
-function Menu(props) {
+const mapStateToProps = (state) => {
+    return { categories: state.categories };
+};
+
+function MenuComponent(props) {
     return(
         <div>
             <ul id="slide-out" className="sidenav sidenav-fixed">
@@ -9,11 +14,15 @@ function Menu(props) {
                 <li><a href="#!"><i className="material-icons teal-text">add_circle</i>Add Post</a></li>
                 <li><div className="divider"></div></li>
                 <li><a className="subheader">Categories</a></li>
-                <li className={props.activeMenu === 'all' ? 'active':''}><a className="waves-effect" href="#!">All Posts</a></li>
+                {props.categories.map((category) => (
+                    <li className={props.activeMenu === 'all' ? 'active':''}><a className="waves-effect" href="#!">All Posts</a></li>
+                ))}
+                
             </ul>
             <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
         </div>
     );
 }
 
+const Menu = connect(mapStateToProps)(MenuComponent);
 export default Menu;
